4aa2191ba0c95f1c360c8caf896723d7ddf91fd6
dongmin <letong1543@kakao.com>
2024-10-30 01:52:50 +0900

    실시간 채팅기능 학습완료, 응용사항은 보류

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { ObjectId } = require('mongodb')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt')
 const MongoStore = require('connect-mongo')
+
+// socket.io 세팅
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const server = createServer(app)
+const io = new Server(server) 
 
 require('dotenv').config()
 
@@ -15,7 +22,6 @@ app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'))
 app.use('/', require('./routes/list.js'))
 app.use('/', require('./routes/user.js'))
-app.use('/', require('./routes/chat.js'))
 app.use(passport.initialize())
 app.use(session({
   secret: process.env.PASSPORT_Secret,
@@ -69,11 +75,12 @@ let connectDB = require('./database.js')
 let db
 connectDB.then((client)=>{
   db = client.db('forum')
-  app.listen(process.env.port, () => console.log('http://localhost:' + process.env.port + ' 서버실행'));
+  server.listen(process.env.port, () => console.log('http://localhost:' + process.env.port + ' 서버실행'));
 }).catch((err)=>{
   console.log(err)
 })
 
+/** Main API */
 app.get('/', (req, res) => {
     try {
         res.render('index.ejs')
@@ -81,4 +88,68 @@ app.get('/', (req, res) => {
         console.log(err);
         res.status(500).send('Server Error')
     }
+})
+
+/** Chat API */
+app.get('/chat/list', async (req, res)=>{
+  try {
+    const result = await db.collection('chatroom').find({
+      member : req.user._id,
+    }).toArray()
+    res.render('chat-list.ejs', {result : result})
+  } catch(err){
+      console.log(err)
+      res.status(500).send('Server error')
+  }
+})
+
+app.get('/chat/request', async (req, res)=>{
+  try {
+    await db.collection('chatroom').insertOne({
+      // member 0:유저아이디, 1:작성자아이디
+      member : [req.user._id, new ObjectId(req.query.writerId)],
+      date : new Date(),
+    })
+    res.redirect('/chat/list')
+  } catch(err){
+      console.log(err)
+      res.status(500).send('Server error')
+  }
+})
+
+app.get('/chat/detail/:id', async (req, res)=>{
+  try {
+    const result = await db.collection('chatroom').findOne({_id : new ObjectId(req.params.id)})
+    res.render('chat-detail.ejs', {result : result})
+  } catch(err){
+      console.log(err)
+      res.status(500).send('Server error')
+  }
+})
+
+io.on('connection', (socket) => {
+
+  /** [유저 -> 서버] 데이터 받아오기
+   * socket.on('데이터이름', (받은 데이터)=>{받은 데이터를 활용할 코드})
+   */
+  socket.on('age', (data)=>{
+    console.log('age : ', data)
+
+  /** [서버 -> 모든유저] 데이터 전송
+   * io.emit('데이터이름', '보낼 데이터')
+   */
+    io.emit('name', 'Lee')
+  })
+
+  /** 모든 유저에게 보내는 것이 아닌, 특정 유저에게 데이터를 전달하려면 room기능을 사용해야함 */
+  // 유저를 room으로 보내기 == socket.join('룸 이름')
+  socket.on('ask-join', (data)=>{
+    socket.join(data);
+    console.log('room 접속 : ', data)
+  })
+
+  socket.on('message-send', (data)=>{
+    // io.emit 사이에 to(룸이름)을 넣으면 특정 룸에 데이터를 보낼 수 있다.
+    io.to(data.room).emit('message-broadcast', data.msg)
+  })
 })
